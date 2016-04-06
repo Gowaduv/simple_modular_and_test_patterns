94375934d071227bb6e63a765b3ad5816cf45c4f
KMSkelton <kmskelton@outlook.com>
2016-04-06 00:24:01 -0700

    add gulp file to run tests and lint; add gulp-eslint; linted for 2 space indent, double quotes, allow console.log; add package.json with devDependencies (no 'dependencies')

--- skelton_code_ass_1/lib/greet.js
+++ skelton_code_ass_1/lib/greet.js
@@ -1,3 +1,4 @@
+"use strict";
 const Greet = module.exports = function(){
 };
 Greet.prototype.response = function(name) {
@@ -7,7 +8,7 @@ Greet.prototype.response = function(name) {
 var cli = new Greet();
 if (process.argv[2]){
   console.log(cli.response(process.argv[2]));
-  return cli.response(process.argv[2]);
 }
 
 // http://stackoverflow.com/questions/4351521/how-do-i-pass-command-line-arguments-to-node-js  and  http://stackoverflow.com/questions/12925802/make-a-node-js-script-accept-command-line-arguments/12925835#12925835
+//
