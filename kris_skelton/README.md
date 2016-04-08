5fda4c03869673c89ff97bfffe417a113e14bee1
KMSkelton <kmskelton@outlook.com>
2016-04-07 21:55:50 -0700

    pre-emptive resubmission to address concerns with assignment 1: folder renamed; index.js added with greet.js exported; comments removed from code. Attempted refactor of code but could not devise updated meaningful tests.

---
+++ kris_skelton/README.md
@@ -0,0 +1,5 @@
+## in terminal (from "lib" directory): 
+node greet "Your Name"
+
+## to run tests and linting from root directory via command line: 
+gulp
---
+++ kris_skelton/build/greet.js
@@ -0,0 +1,14 @@
+"use strict";
+const Greet = module.exports = function(){
+};
+Greet.prototype.response = function(name) {
+  var toScreen = ("Hello, " + name || "world!");
+  return toScreen;
+};
+var cli = new Greet();
+if (process.argv[2]){
+  console.log(cli.response(process.argv[2]));
+}
+
+// http://stackoverflow.com/questions/4351521/how-do-i-pass-command-line-arguments-to-node-js  and  http://stackoverflow.com/questions/12925802/make-a-node-js-script-accept-command-line-arguments/12925835#12925835
+//
